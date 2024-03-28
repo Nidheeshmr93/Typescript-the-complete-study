35e6b25a9b1aa2ee8e0778423f53cbedf938cf95
Nidheeshmr93 <nidheeshmr93@gmail.com>
2024-03-28 19:16:41 +0530

    Annotatations with variables.

---
+++ section-3-type-annotations-in-action/features/variables.ts
@@ -0,0 +1,19 @@
+let apples: number = 5; // this variable must be a number
+
+// apples= "ssasa";
+
+let speed: string = 'fast'; // this variable must be a string
+
+//speed = 10;
+
+let hasName: boolean = true;
+
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+
+
+//built in objects
+
+let now: Date = new Date();
