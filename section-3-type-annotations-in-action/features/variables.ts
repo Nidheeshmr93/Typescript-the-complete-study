41e6170db332cbd15880bf70ad19963c956a52a3
Nidheeshmr93 <nidheeshmr93@gmail.com>
2024-04-01 17:23:37 +0530

    Type annotations is completed.

--- section-3-type-annotations-in-action/features/variables.ts
+++ section-3-type-annotations-in-action/features/variables.ts
@@ -17,3 +17,36 @@ let nothing: undefined = undefined;
 //built in objects
 
 let now: Date = new Date();
+
+// array
+
+let colors: string[] = ['red','green', 'blue'];
+
+let myNumbers: number[]  = [1,2,3];
+
+let truths: boolean [] = [ true, true,false ];
+
+
+// Classes
+
+class Car {
+
+}
+
+let car: Car =  new Car();
+
+
+// object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+}
+
+
+// Function
+
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+}
