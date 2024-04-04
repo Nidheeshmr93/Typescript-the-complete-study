557ccc3883f27793ffa555ab98414b72c8e8416e
Nidheeshmr93 <nidheeshmr93@gmail.com>
2024-04-04 17:25:00 +0530

    The type annotations is completed.

--- section-3-type-annotations-in-action/features/variables.ts
+++ section-3-type-annotations-in-action/features/variables.ts
@@ -50,3 +50,38 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 }
+
+
+// When to use annotations
+// 1) Function that return the 'any' type
+
+const json  = '{"x": 10, "y": 20}';
+const coordinates: {x: number; y: number} = JSON.parse(json);
+console.log(coordinates); // {x: = 10, y: 20}
+
+
+// 2) when we declare a variable on one line and initalizate it later
+
+let words = ['red','green','blue'];
+let foundWord: boolean;
+
+for(let i = 0; i<words.length; i++) {
+
+  if(words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+
+let numbers = [-10,-1,-12];
+let numberAboveZero: boolean | number ;
+
+
+for(let i =0; i< numbers.length; i++) {
+
+  if(numbers[i]) {
+
+    numberAboveZero =  numbers[i];
+  }
+}
